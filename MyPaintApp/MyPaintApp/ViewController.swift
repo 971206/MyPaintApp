453b2d8145d907e8b286859a289fd8c8e6c31d5e
MacBook <>
2021-07-01 12:20:45 +0400

    initial commit

---
+++ MyPaintApp/MyPaintApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPaintApp
+//
+//  Created by MacBook  on 01.07.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
